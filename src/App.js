11d979078c638425daa277aa7f39af41b1c9c7aa
valeriapaulinalustres <valeriapaulinalustres@yahoo.com>
2022-05-19 22:22:59 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola Gaby</h1>
+      <h2>Esta vez nos va a salir súper!</h2>
+    </div>
+  );
+}
+
+export default App;
